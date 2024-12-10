07ac21ae29c54fc5269515bac03a6085a1564563
PaulaUc3m <100472119@alumnos.uc3m.es>
2024-12-10 10:38:12 +0100

    correccion predict 3 clases

--- scripts/YOLO_3class/yolo_3class_predict.py
+++ scripts/YOLO_3class/yolo_3class_predict.py
@@ -14,6 +14,7 @@ format = {   0: 'correct_set',
              2: 'waiting'}
 correct = 0
 wrong = 0
+waiting = 0
 confidence = []
 
 
@@ -24,8 +25,18 @@ for prediction in predictions:
         wrong +=1
     elif top == 0:
         correct +=1
+    else:
+        waiting +=1
 
-print("RESULTADO: set correcto =", correct, " instancias, porcentaje = ", 100*(correct/(correct+wrong)), "%\n",
-      "set incorrecto= ",wrong, "instancias, porcentaje =", 100*(wrong/(wrong+correct)), 
+
+if correct > wrong:
+    clase_mayoritaria = "Set correcto"
+elif wrong > correct:
+    clase_mayoritaria = "Set incorrecto"
+else:
+    clase_mayoritaria = "no concluyente"
+
+print("RESULTADO: set correcto =", correct, " instancias, porcentaje = ", 100*(correct/(correct+wrong+waiting)), "%\n",
+      "set incorrecto= ",wrong, "instancias, porcentaje =", 100*(wrong/(wrong+correct+waiting)), 
       "\nConfianza media:",np.mean(confidence),
-      "Clase mayoritaria de clasificación:", max(correct,wrong))
+      "Clase mayoritaria de clasificación:", clase_mayoritaria)
